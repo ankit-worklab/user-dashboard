36d0e5176891a9daa33718b45924b764a9e01a7c
ankit <ankitnathbpl@gmail.com>
2023-12-14 23:43:35 +0530

    spring boot project is created

---
+++ userHandlingApi/src/main/java/com/mylab/UserHandlingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHandlingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHandlingApiApplication.class, args);
+	}
+
+}
